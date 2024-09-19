a3e2eb6a344493ceef23192917d86653e6dea913
John Doe <john.doe@.org>
2024-09-19 12:45:06 +0200

    Added environment variable handling

--- internal/config/config.go
+++ internal/config/config.go
@@ -1,18 +1,32 @@
 package config
 
-import "flag"
+import (
+	"flag"
+
+	"github.com/caarlos0/env"
+	"github.com/gofiber/fiber/v2/log"
+)
 
 type Config struct {
-	Address string
-	BaseURL string
+	Address string `env:"SERVER_ADDRESS"`
+	BaseURL string `env:"BASE_URL"`
 }
 
 func NewConfig() *Config {
 	cfg := &Config{}
 
-	flag.StringVar(&cfg.Address, "a", "localhost:8080", "HTTP-Server address")
-	flag.StringVar(&cfg.BaseURL, "b", "http://localhost:8080",
-		"Base URL for shortened links")
+	if err := env.Parse(cfg); err != nil {
+		log.Error(err)
+	}
+
+	if cfg.Address == "" {
+		flag.StringVar(&cfg.Address, "a", "localhost:8080", "HTTP-Server address")
+	}
+
+	if cfg.BaseURL == "" {
+		flag.StringVar(&cfg.BaseURL, "b", "http://localhost:8080",
+			"Base URL for shortened links")
+	}
 	flag.Parse()
 
 	return cfg
